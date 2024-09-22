f6a1c7cc575870763a2475b35848bdc58e89d927
Miguel Acero <miguelacero528@gmail.com>
2024-09-22 17:22:02 -0400

    changes

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,15 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	lazy = false,
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-			ensure_installed = { "markdown", "markdown_inline" },
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  -- lazy = false,
+  event = "BufRead",
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+      ensure_installed = { "markdown", "markdown_inline" },
+    })
+  end,
 }
