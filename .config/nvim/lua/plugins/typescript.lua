ac210191e2bc3d472a8a947ae95e646488b3a279
Miguel Acero <miguelacero528@gmail.com>
2024-09-22 17:22:02 -0400

    changes

--- .config/nvim/lua/plugins/typescript.lua
+++ .config/nvim/lua/plugins/typescript.lua
@@ -1,24 +1,19 @@
 return {
-  "pmizio/typescript-tools.nvim",
-  dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-  opts = {},
-  config = function()
-    local keymap = vim.keymap.set
-    local opts = { noremap = true, silent = true }
-    local api = require("typescript-tools.api")
-    require("typescript-tools").setup({
-      handlers = {
-        ["textDocument/publishDiagnostics"] = api.filter_diagnostics({ 80006, 1540 }),
-      },
-    })
-    keymap("n", "<leader>ro", ":TSToolsOrganizeImports<CR>", opts)
-    keymap("n", "<leader>ru", ":TSToolsRemoveUnusedImports<CR>", opts)
-    keymap("n", "<c-l>", ":TSToolsAddMissingImports<CR>", opts)
-    -- keymap("n", "<leader>tami", ":TSToolsAddMissingImports<CR>", opts)
-    -- keymap("n", "<leader>tgsd", ":TSToolsGoToSourceDefinition<CR>", opts)
-    -- keymap("n", "<leader>trf", ":TSToolsRenameFile<CR>", opts)
-    -- keymap("n", "<leader>tfr", ":TSToolsFileReferences<CR>", opts)
-    -- keymap("n", "<leader>tru", ":TSToolsRemoveUnused<CR>", opts)
-    -- keymap("n", "<leader>tsi", ":TSToolsSortImports<CR>", opts)
-  end,
+	"pmizio/typescript-tools.nvim",
+	lazy = false,
+	dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+	opts = {},
+	config = function()
+		local keymap = vim.keymap.set
+		local opts = { noremap = true, silent = true }
+		local api = require("typescript-tools.api")
+		require("typescript-tools").setup({
+			handlers = {
+				["textDocument/publishDiagnostics"] = api.filter_diagnostics({ 80006, 1540 }),
+			},
+		})
+		keymap("n", "<leader>ro", ":TSToolsOrganizeImports<CR>", opts)
+		keymap("n", "<leader>ru", ":TSToolsRemoveUnusedImports<CR>", opts)
+		keymap("n", "<c-l>", ":TSToolsAddMissingImports<CR>", opts)
+	end,
 }
