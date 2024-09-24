e811a472b99ec69a62ad42852b3e95e7ce1960d5
Miguel Acero <miguelacero528@gmail.com>
2024-09-23 23:23:46 -0400

    changes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,44 +1,50 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
-			{ "\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out,                            "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 require("mappings")
 require("lazy").setup({
-	defaults = { lazy = true },
-	spec = {
-		{ import = "plugins" },
-	},
-	install = { colorscheme = { "ayu" } },
-	checker = { enabled = true },
-	ui = { border = "rounded" },
-	performance = {
-		cache = {
-			enabled = true,
-		},
-		rtp = {
-			disabled_plugins = {
-				"netrwPlugin",
-				"gzip",
-				"tarPlugin",
-				"tohtml",
-				"tutor",
-				"zipPlugin",
-			},
-		},
-	},
-	debug = false,
+  defaults = { lazy = true },
+  spec = {
+    { import = "plugins" },
+  },
+  install = { colorscheme = { "ayu" } },
+  checker = {
+    enabled = false,
+    concurrency = nil,
+    notify = true,
+    frequency = 3600,
+    check_pinned = false,
+  },
+  ui = { border = "rounded" },
+  performance = {
+    cache = {
+      enabled = true,
+    },
+    rtp = {
+      disabled_plugins = {
+        "netrwPlugin",
+        "gzip",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+  debug = false,
 })
