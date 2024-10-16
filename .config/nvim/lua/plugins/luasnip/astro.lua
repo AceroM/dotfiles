1da475114aa82f86db518ec9d6f0825fcc69334d
Miguel Acero <miguelacero528@gmail.com>
2024-10-16 12:48:56 -0400

    changes

--- .config/nvim/lua/plugins/luasnip/astro.lua
+++ .config/nvim/lua/plugins/luasnip/astro.lua
@@ -2,7 +2,6 @@ local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
-
 return {
-  s("c=", { t('class="'), i(1), t('"') }),
+	s("c=", { t('class="'), i(1), t('"') }),
 }
