cf74cc0f81a847a416b58b1784e04f501064083e
Miguel Acero <miguelacero528@gmail.com>
2025-04-30 11:54:16 -0400

    changes

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,12 +1,21 @@
 return {
 	{
-		"Shatur/neovim-ayu",
-		lazy = false,
-		name = "ayu",
+		"catppuccin/nvim",
+		name = "catppuccin",
 		priority = 1000,
 		config = function()
-			require("ayu").setup({ mirage = true })
-			vim.cmd([[colorscheme ayu-mirage]])
+			require("catppuccin").setup()
+			vim.cmd([[colorscheme catppuccin-latte]])
 		end,
 	},
+	-- {
+	-- 	"Shatur/neovim-ayu",
+	-- 	lazy = false,
+	-- 	name = "ayu",
+	-- 	priority = 1000,
+		-- config = function()
+		-- 	require("ayu").setup({ mirage = true })
+		-- 	vim.cmd([[colorscheme ayu-mirage]])
+		-- end,
+	-- },
 }
