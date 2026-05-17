3b6fbacf2f0509cab696f6b30f94ca6d1cb32521
Miguel Acero <AceroM@users.noreply.github.com>
2026-05-17 15:08:49 -0400

    changes

--- zsh/.config/zsh/ripgrep.zsh
+++ zsh/.config/zsh/ripgrep.zsh
@@ -1,1 +1,12 @@
 alias rg='rg --hidden --glob "!.git"'
+ra() {
+  local dir=$PWD
+  while [[ $dir != / ]]; do
+    if [[ -f $dir/.raignore ]]; then
+      rg -uu --ignore-file "$dir/.raignore" "$@"
+      return
+    fi
+    dir=${dir:h}
+  done
+  rg -uu --glob '!node_modules' "$@"
+}
