c8a988eaf68fe9a99f2f7e071374b0c82e520074
Miguel Acero <AceroM@users.noreply.github.com>
2026-05-29 17:08:44 -0400

    changes

--- zsh/.config/zsh/doppler.zsh
+++ zsh/.config/zsh/doppler.zsh
@@ -1,2 +1,8 @@
-function dp() { doppler "$@" }
+dp() {
+  if [[ -f ./scripts/deploy.sh ]]; then
+    ./scripts/deploy.sh "$@"
+  else
+    doppler "$@"
+  fi
+}
 function ds() { doppler secrets get "$@" --plain }
