794f00a67bfcb1f7414cd6ff25a30e659b0fa2c2
Miguel Acero <AceroM@users.noreply.github.com>
2026-06-25 23:01:51 -0400

    changes

--- neovim/.config/nvim/lua/plugins/typescript.lua
+++ neovim/.config/nvim/lua/plugins/typescript.lua
@@ -1,7 +1,7 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
-    inlayHints = { enabled = false },
+    inlay_hints = { enabled = false },
     servers = {
       --- @deprecated -- tsserver renamed to ts_ls but not yet released, so keep this for now
       --- the proper approach is to check the nvim-lspconfig release version when it's released to determine the server name dynamically
