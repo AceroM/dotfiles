a69adc0e2a55457b5eb81929cb2320a2e77bf50f
Miguel Acero <AceroM@users.noreply.github.com>
2026-08-13 15:07:22 -0400

    changes

--- zsh/.config/zsh/ripgrep.zsh
+++ zsh/.config/zsh/ripgrep.zsh
@@ -1,4 +1,16 @@
 alias rg='rg --hidden --glob "!.git"'
+
+# Open the first ripgrep match in Neovim at its matching line.
+# Usage: rv 'tick rows' [rg options]
+rv() {
+  local match file remainder line
+  match=$(rg --no-heading --line-number --max-count 1 "$@") || return
+  file=${match%%:*}
+  remainder=${match#*:}
+  line=${remainder%%:*}
+  nvim "+$line" "$file"
+}
+
 ra() {
   local dir=$PWD
   while [[ $dir != / ]]; do
